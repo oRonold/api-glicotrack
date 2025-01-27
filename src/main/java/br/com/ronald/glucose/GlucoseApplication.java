6b173483b7a02ba16f985a069fd8b9c5229d07e6
oRonold <or040918@gmail.com>
2025-01-27 14:57:43 -0300

    initial commit

---
+++ src/main/java/br/com/ronald/glucose/GlucoseApplication.java
@@ -0,0 +1,13 @@
+package br.com.ronald.glucose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlucoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlucoseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ronald/glucose/GlucoseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ronald.glucose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlucoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
