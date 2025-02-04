05ba29089c49631577072bc50d350759a2311f8f
oRonold <or040918@gmail.com>
2025-02-04 11:44:58 -0300

    Implementing pacient service

---
+++ src/main/java/br/com/ronald/glucose/service/PacientService.java
@@ -0,0 +1,23 @@
+package br.com.ronald.glucose.service;
+
+import br.com.ronald.glucose.model.Pacient;
+import br.com.ronald.glucose.model.dto.PacientEnrollDTO;
+import br.com.ronald.glucose.repository.PacientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PacientService {
+
+    @Autowired
+    private PacientRepository repository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    public Pacient enroll(PacientEnrollDTO dto){
+        Pacient pacient = new Pacient(dto, encoder.encode(dto.password()));
+        return repository.save(pacient);
+    }
+}
