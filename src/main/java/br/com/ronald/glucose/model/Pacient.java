02cc2b270ae1bb0d9efdf6b4d1cc833d99043d92
oRonold <or040918@gmail.com>
2025-02-24 14:55:30 -0300

    Removing some unused importations

--- src/main/java/br/com/ronald/glucose/model/Pacient.java
+++ src/main/java/br/com/ronald/glucose/model/Pacient.java
@@ -3,14 +3,10 @@ package br.com.ronald.glucose.model;
 import br.com.ronald.glucose.model.dto.PacientEnrollDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@NoArgsConstructor
 @AllArgsConstructor
 
 @Entity
