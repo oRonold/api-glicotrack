20aca5490d88907d44d9f1e0ba204ebf18c0caeb
oRonold <or040918@gmail.com>
2025-03-25 15:38:14 -0300

    Sending token to web as a cookie

--- src/main/java/br/com/ronald/glucose/service/LoginService.java
+++ src/main/java/br/com/ronald/glucose/service/LoginService.java
@@ -3,10 +3,13 @@ package br.com.ronald.glucose.service;
 import br.com.ronald.glucose.model.Pacient;
 import br.com.ronald.glucose.model.dto.LoginDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 public class LoginService {
@@ -20,7 +23,15 @@ public class LoginService {
     public String login(LoginDTO dto){
         UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
         Authentication authentication = authenticationManager.authenticate(user);
-        return tokenService.createToken((Pacient) authentication.getPrincipal());
+        String token = tokenService.createToken((Pacient) authentication.getPrincipal());
+        ResponseCookie cookie = ResponseCookie.from("token", token)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("Strict")
+                .maxAge(Duration.ofHours(2))
+                .path("/")
+                .build();
+        return cookie.toString();
     }
 
 }
